a8921f88e41598edbc5c72f70c322bbf6c0214c8
rosellelogrono5-commits <rosellelogrono5@gmail.com>
2026-04-21 09:00:15 +0800

    Delete supabase directory

--- supabase/migrations/20260413080041_fc35c5b9-5260-4302-9f0f-1180dce78904.sql
+++
@@ -1,3 +0,0 @@
-
-DROP POLICY "System can create notifications" ON public.notifications;
-CREATE POLICY "Users can create notifications" ON public.notifications FOR INSERT TO authenticated WITH CHECK (auth.uid() = user_id OR public.has_role(auth.uid(), 'hrdd_admin') OR public.has_role(auth.uid(), 'supervisor'));
